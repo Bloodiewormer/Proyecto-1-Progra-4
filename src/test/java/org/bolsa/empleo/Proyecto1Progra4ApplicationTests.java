94fd615dc6d4a66318f9b57477707c7f9e45c5ca
Bloodiewormer <dbgc977@gmail.com>
2026-03-14 19:18:20 -0600

    add index.html with basic HTML structure

---
+++ src/test/java/org/bolsa/empleo/Proyecto1Progra4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bolsa.empleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1Progra4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
